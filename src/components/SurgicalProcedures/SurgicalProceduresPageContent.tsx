1694a2a202359681ee1db03e16b9908b2179fd4f
Kuba <kubasamulski1@gmail.com>
2024-08-20 22:16:45 +0200

    changed title and updated content

--- src/components/SurgicalProcedures/SurgicalProceduresPageContent.tsx
+++ src/components/SurgicalProcedures/SurgicalProceduresPageContent.tsx
@@ -1,19 +1,16 @@
 export const removalOfSkinTitle = "Usuwanie zmian skórnych";
-export const removalOfSkinContent = `Zmiany skórne to znamiona, pieprzyki oraz brodawki. Często usuwane są również włókniaki miękkie, które są nieszkodliwymi nowotworami tkanki łącznej oraz położone pod skórą zmiany guzkowe -najczęściej kaszaki i tłuszczaki. 
-Wybór metody usuwania zmian skórnych jest zależny od ich kategoryzacji. Zabiegi wykonywane są zarówno z powodów estetycznych, jak też ze względu na wskazania medyczne. 
-Najbardziej popularnymi technikami zabiegowymi, stosowanymi w usuwaniu zmian skórnych, są metoda  chirurgiczna. lub laserowa. Tylko przy usunięciu chirurgicznym możliwa jest ocena histopatologiczna!!
-Wycięcie chirurgiczne - wyłącznie tym sposobem usuwane są zmiany barwnikowe melanocytowe (barwnikowe), z których może rozwinąć się czerniak. Tzw. szerokie wycięcie (wide excizion) jest postępowaniem podstawowym w terapii nowotworów złośliwych skóry. Oprócz zmian barwnikowych, metodą chirurgicznego wycięcia usuwa się m.in. tłuszczaki, plamy soczewicowate czy znamiona naczyniowe (tzw. naczyniaki). Zabieg wykonywany jest w znieczuleniu miejscowym- jest całkowicie niebolesny.  Na ranę zakładane są szwy, które są zdejmowane po 5-14 dni w zależności od potrzeby. 
-Badanie histopatlogiczne jest zalecane ale w niektórych oczywistych przypadkach niekonieczne.
+export const removalOfSkinContent = `<strong><u>Zmiany skórne i zmiany podskórne</u></strong>, które wymagają usunięcia to różnego rodzaju znamiona, popularne „pieprzyki” czyli brodawki oraz guzki tkanki podskórnej, np. tłuszczaki, kaszaki czy włókniaki. Często usuwane są także <strong><u>włókniaki miękkie</u></strong>, czyli małe, czasem liczne zmiany, które są zlokalizowane w okolicy szyi, pach i pachwin. Wiele z nich wymaga usunięcia ze względów zdrowotnych, zwykle po konsultacji lekarza dermatologa, jako podejrzenie raka skóry lub jako profilaktyka zmian przedrakowych.
+    
+    Zmiany określone jako łagodne często wymagają usunięcia z przyczyn estetycznych. Wybór metod usuwania zmian skórnych zależy od ich kategoryzacji. Zabiegi chirurgiczne dają pewność i gwarancję, że usunięta zmiana zostanie zbadana histopatologicznie (decyzja o badaniu należy do pacjenta, jednak zalecam takie badanie każdej zmiany) i mamy 100% pewności, że była łagodna lub nakazuje pilne leczenie, gdyby wynik był niepokojący.
+    
+    Alternatywą jest leczenie np. laserem, co daje podobny efekt wizualny – jednak nie pozwala to na zbadanie zmiany, więc nie wiemy, co usunęliśmy. Taki zabieg jest zalecany <strong><u>TYLKO</u></strong> przy absolutnej pewności dermatologa, że zmiana jest łagodna! Zabieg wykonywany jest w znieczuleniu miejscowym – jest całkowicie niebolesny.
+    
+    Na ranę zakładane są szwy, które są usuwane po 5-14 dniach w zależności od rodzaju i umiejscowienia zmiany. Blizna po usunięciu zależy od wielkości zmiany i właściwości skóry pacjenta.
 
-<u>cena za usunięcie:</u>
-1-2 zmian małych - 400 zł 
-3-4 zmian małych - 500 zł
-powyżej 5 zmian cena za zabieg ustalana jest indywidualnie.
-duże zmiany- po konsultacji - cena w zależności od złożoności zabiegu.
+       <u>Cena:</u><ul> <li>1-2 zmiany małe – 450 zł</li><li>3-4 zmiany małe – 550 zł</li><li>Powyżej 5 zmian – cena ustalana indywidualnie</li><li>Duże zmiany lub z głębszych tkanek – cena ustalana indywidualnie</li></ul>
+    <strong><u>Badanie histopatologiczne</u></strong> – 60 zł za badanie 1 zmiany (aktualna cena może się zmienić i jest zależna od wyceny przez zakład patomorfologii – obecnie jest w ośrodku w Poznaniu).
 
-
-<u>Badanie histopatologiczne</u> obecnie cena -100zł za zbadanie 1 zmiany (aktualna cena zależy od wyceny przez zakład patomorfologii -obecnie jest to ośrodek w Poznaniu) 
-    `
+    <strong><u>WAŻNE</u></strong> – podczas oceny usuniętej zmiany Zakład Patomorfologii w szczególnych przypadkach jest zmuszony do wykonania bardzo specjalistycznych testów immunohistochemicznych, aby dokładnie ustalić rozpoznanie. Jest to niezwykle istotne przy diagnostyce zmian rakowych, czerniaka lub chłoniaka. Wtedy cena może wzrosnąć do kilkuset złotych (zgodnie z przesłaną fakturą) i zapłata za nią jest kosztem/obciążeniem pacjenta!`
 
 export const scarCorrectionTitle = "Korekta blizn"
 export const scarCorrectioncontent = `Uszkodzenie skóry w wyniku urazu lub zabiegu chirurgicznego pozostawia po sobie  blizny. Blizny, które po upływie czasu pozostały szpecące można skorygować poprzez zabieg chirurgiczny. Zabieg wykonuje się w znieczuleniu miejscowym, a jego długość uzależniona jest od wielkości blizny. W przypadku mniej skomplikowanych blizn możliwe jest zastosowanie zabiegu wycięcia i zszycia na nowo starej blizny. Należy pamiętać, że nie zniweluje on całkowicie blizny, może jedynie ją skorygować, zmniejszyć jej widoczność. Nie każda blizna nadaje się do usunięcia- decyzja i przedstawienie możliwości korekcji jest indywidualna. 
